0ce2015ea44e3f49bb1539535153fea8864f01df
SIjey2508 <vsijey2508@gmail.com>
2018-01-12 13:31:21 +0000

    Add Connect

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
-
+const socketIO = require('socket.io')
 const app = express()
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
+
+var server = http.createServer((app))
+var io = socketIO(server)
+
+
+io.on('connection', (socket) => {
+    console.log('New User connected')
+
+    socket.on('disconnect', () => {
+        console.log('User was  Disconnected')
+    })
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`You're Connected to port ${port}`)
 })
-app.get('/', (req, res, next) => {
-    res.send('This is a test')
-    })
-
  
